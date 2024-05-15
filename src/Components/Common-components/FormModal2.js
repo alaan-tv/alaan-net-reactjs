eeca3ba7352488ea22ed873c7983fe59bdf71867
Irshad Ansari <irshad.ansari@alaan.tv>
2024-05-15 18:03:33 +0400

    fix close form issue in SBD

--- src/Components/Common-components/FormModal2.js
+++ src/Components/Common-components/FormModal2.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const FormModal2 = ({ handleClose, showForm, children }) => {
+const FormModal2 = ({ handleClose1, showForm, children }) => {
 	const modelClass = showForm ? 'form-modal open' : 'form-modal';
 	return (
 		<div className={modelClass}  id="form-one">
 			<div className="form-modal-bg form-modal-exit"></div>
 			<div className="form-modal-container">
 				{children}
-				<button className="form-modal-close form-modal-exit" onClick={handleClose}>&times;</button>
+				<button className="form-modal-close form-modal-exit" onClick={handleClose1}>&times;</button>
 			</div>
 		</div>
 	)
